963c2be2e96c2d936c6601368b6393af8ef4786c
matheus-giordani <matheus.giordanioliveira@gmail.com>
2023-07-10 00:04:54 -0300

    style:

--- test/common/api-error-handler.filter.test.ts
+++ test/common/api-error-handler.filter.test.ts
@@ -1,43 +1,40 @@
 import { ArgumentsHost, HttpAdapterHost } from '@nestjs/common';
 import { ApiErrorExceptionFilter } from './../../src/common/error-handler/api-error-handler.filter';
 import { ApiError } from './../../src/utils/MsgError';
-;
-
 describe('ApiErrorExceptionFilter', () => {
-    let filter: ApiErrorExceptionFilter;
-    let httpAdapterHost: HttpAdapterHost;
-    let mockAdapter: any;
-    let mockHost: ArgumentsHost;
-    let mockResponse: any;
-    let mockException: ApiError;
-  
-    beforeEach(() => {
-      mockAdapter = {
-        reply: jest.fn(),
-      };
-      mockResponse = {};
-      mockException = new ApiError([{cause:'Error message'}], 400);
-  
-      httpAdapterHost = {
-        httpAdapter: mockAdapter,
-      };
-  
-      mockHost = {
-          switchToHttp: jest.fn().mockReturnThis(),
-          getResponse: jest.fn().mockReturnValue(mockResponse),
-      } as unknown as ArgumentsHost;
-  
-      filter = new ApiErrorExceptionFilter(httpAdapterHost);
-    });
-  
-    it('should catch the exception and reply with the appropriate response', () => {
-      filter.catch(mockException, mockHost);
-  
-      expect(mockAdapter.reply).toHaveBeenCalledWith(
-        mockResponse,
-        {"httpStatus": 400, "message": [{"cause": "Error message"}]},
-        400,
-      );
-    });
+  let filter: ApiErrorExceptionFilter;
+  let httpAdapterHost: HttpAdapterHost;
+  let mockAdapter: any;
+  let mockHost: ArgumentsHost;
+  let mockResponse: any;
+  let mockException: ApiError;
+
+  beforeEach(() => {
+    mockAdapter = {
+      reply: jest.fn(),
+    };
+    mockResponse = {};
+    mockException = new ApiError([{ cause: 'Error message' }], 400);
+
+    httpAdapterHost = {
+      httpAdapter: mockAdapter,
+    };
+
+    mockHost = {
+      switchToHttp: jest.fn().mockReturnThis(),
+      getResponse: jest.fn().mockReturnValue(mockResponse),
+    } as unknown as ArgumentsHost;
+
+    filter = new ApiErrorExceptionFilter(httpAdapterHost);
   });
 
+  it('should catch the exception and reply with the appropriate response', () => {
+    filter.catch(mockException, mockHost);
+
+    expect(mockAdapter.reply).toHaveBeenCalledWith(
+      mockResponse,
+      { httpStatus: 400, message: [{ cause: 'Error message' }] },
+      400,
+    );
+  });
+});
